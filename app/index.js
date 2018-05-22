16536d975f4e2b4d88e02486d937ebf7649e6b91
suoyue <1255915270@qq.com>
2018-05-22 17:37:49 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,28 @@
+/**
+ * homePage
+ */
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ImageBackground,
+    Dimensions,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+
+type Props = {};
+export default class HomePage extends Component<Props> {
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    拾谷
+                </Text>
+            </View>
+        );
+    }
+}
